2b2a8e193392b095dc625d9bd1677d9adac89b00
José Marcelo Rosiles Cortés <josemarcelorosilescortes@gmail.com>
2021-11-25 17:57:00 -0600

    google analytics

--- app.py
+++ app.py
@@ -21,6 +21,36 @@ app = dash.Dash(__name__, title='GeoSTATS',
 				meta_tags=[{'name': 'viewport',
                              'content': 'width=device-width, initial-scale=1.0'},])
 
+# GOOGLE ANALYTICS TAGS
+app.index_string = '''
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-XLVYFYJ63V"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'G-XLVYFYJ63V');
+		</script>
+        {%metas%}
+        <title>{%title%}</title>
+        {%favicon%}
+        {%css%}
+    </head>
+    <body>
+        {%app_entry%}
+        <footer>
+            {%config%}
+            {%scripts%}
+            {%renderer%}
+        </footer>
+    </body>
+</html>
+'''
+
 server = app.server
 
 # DATOS
