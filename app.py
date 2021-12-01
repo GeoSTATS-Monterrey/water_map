af863981bd67d8b220fc768ff5819bc81b7a47f6
José Marcelo Rosiles Cortés <josemarcelorosilescortes@gmail.com>
2021-11-30 19:28:08 -0600

    ups

--- app.py
+++ app.py
@@ -437,15 +437,15 @@ app.layout = html.Div([
 #         return not is_open
 #     return is_open
 
-@app.callback(
-    Output("modal", "is_open"),
-    [Input("open", "n_clicks"), Input("close", "n_clicks")],
-    [State("modal", "is_open")],
-)
-def toggle_modal(n1, n2, is_open):
-    if n1 or n2:
-        return not is_open
-    return is_open
+# @app.callback(
+#     Output("modal", "is_open"),
+#     [Input("open", "n_clicks"), Input("close", "n_clicks")],
+#     [State("modal", "is_open")],
+# )
+# def toggle_modal(n1, n2, is_open):
+#     if n1 or n2:
+#         return not is_open
+#     return is_open
 
 if __name__ == '__main__':
     app.run_server(debug=True)
